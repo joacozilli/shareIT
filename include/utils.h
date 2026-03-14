6eaf84eb0789d0fe5b36117e0539b754e3056aae
joaquin zilli <joacozilli2001@hotmail.com>
2026-03-14 16:36:34 -0300

    deleted eprintf and errnoprintf from utils

--- include/utils.h
+++ include/utils.h
@@ -2,37 +2,9 @@
 #define __UTILS_H__
 
 #include <stddef.h>
-#include <errno.h>
 #include <stdio.h>
-#include <pthread.h>
 
 #include "array.h"
-
-// mutex for printing error messages
-extern pthread_mutex_t eprintf_mutex;
-
-
-// macro for printing errors on stderr. Thread safe.
-#define eprintf(fmt,...) { \
-    pthread_mutex_lock(&eprintf_mutex); \
-    fprintf(stderr, fmt, __VA_ARGS__); \
-    pthread_mutex_unlock(&eprintf_mutex); \
-}
-
-// macro for printing errors on stderr using errno. Thread safe.
-#define errnoprintf(fmt, ...) { \
-    char _errbuf[256]; \
-    safe_strerror1(_errbuf, sizeof _errbuf); \
-    eprintf(fmt, ##__VA_ARGS__); \
-    eprintf(": %s\n", _errbuf); \
-}
-
-/**
- * thread-safe function to extract errno value (strerror uses internal buffer, 
- * and is only MT-safe in glibc >= 2.32). Store the string describing errno value
- * in the passed buffer. Return 0 on success, otherwise non zero.
- */
-int safe_strerror1(char* errbuff, size_t bufflen);
 
 /**
  * Parse string input into tokens based on delim and return array with the tokens.
--- src/utils.c
+++ src/utils.c
@@ -3,21 +3,6 @@
 #include <string.h>
 #include <stdio.h>
 
-pthread_mutex_t eprintf_mutex = PTHREAD_MUTEX_INITIALIZER;
-
-
-int safe_strerror1(char* errbuff, size_t bufflen) {
-    #if defined(__GLIBC__) && defined(_GNU_SOURCE)
-        /* GNU-specific */
-        strerror_r(errno, errbuf, buflen);
-        return 0;
-    #else
-        /* POSIX */
-        int ret = strerror_r(errno, errbuff, bufflen);
-        return ret;
-    #endif
-}
-
 Array parse_input(char* input, char* delim) {
     char* token = strtok(input, delim);
     if (token == NULL)
